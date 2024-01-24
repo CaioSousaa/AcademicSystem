b64a0573bf17e6b205936fffef64686696f17890
Caio <caiothalles607@gmail.com>
2024-01-24 16:17:14 -0300

    fix(data-source): created for config for migrations

--- src/data-source.ts
+++ src/data-source.ts
@@ -11,6 +11,8 @@ const appDataSource = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  entities: [`${__dirname}/**/entities/*.{ts,js}`],
+  migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
 });
 
 export { appDataSource };
